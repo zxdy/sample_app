2dc1c06ead675b8ce92162a8100168d6a8ee533b
zxdy <zxdy@vip.qq.com>
2014-09-30 14:31:56 +0800

    add contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,7 +7,6 @@ describe "Static pages" do
     it { should have_content('Sample App') }
     it { should have_title("Ruby on Rails Tutorial Sample App") }
     it { should_not have_title('| Home') }
-
   end
 
   describe "Help page" do
@@ -23,5 +22,13 @@ describe "Static pages" do
 
     it { should have_content('About') }
     it { should have_title(full_title('About Us')) }
+  end
+
+  describe "Contact page" do
+
+    before { visit contact_path }
+
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact Me')) }
   end
 end
