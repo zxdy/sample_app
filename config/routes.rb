a23cabba8f1b255278f98d3b7322f1b2db041d5c
Ario <zxdy@vip.qq.com>
2014-10-14 22:58:24 +0800

    change route order

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       get :following, :followers
     end
   end
+  match '/jobs/search', to: 'jobs#search',      via:'get'
   resources  :jobs
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
@@ -16,7 +17,6 @@ Rails.application.routes.draw do
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/reprocess', to: 'static_pages#reprocess', via: 'get'
   match '/failed',  to: 'jobs#index',            via: 'get'
-  match '/jobs/search', to: 'jobs#search',      via:'get'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
