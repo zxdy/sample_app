c13cf469a928d792fd7dcbaf8f7034f6ed1abcc4
Ario <zxdy@vip.qq.com>
2014-10-15 21:24:59 +0800

    remove access control from reprocess

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,4 @@
 class JobsController < ApplicationController
-  before_action :signed_in_user
   def index
     @jobs = Job.paginate(page: params[:page])
   end
@@ -11,8 +10,8 @@ class JobsController < ApplicationController
   def search
     @jobs = Job.where("logdate between :start_date AND :start_date",{start_date: params[:log_date]}).paginate(page: params[:page],per_page:10)
     respond_to do |format|
-      format.html
-      format.js
+        format.html
+        format.js
     end
   end
   def new
