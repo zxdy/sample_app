68885d54afb93fdab7e71bc263e032ea5c25af7e
Ario <zxdy@vip.qq.com>
2014-11-08 19:58:39 +0800

    oracle conn

--- app/tools/oracleConn.rb
+++ app/tools/oracleConn.rb
@@ -1,7 +1,7 @@
 require 'oci8'
 
 conn_bts = OCI8.new('tantan/oracle@//192.168.231.128:1521/orcl')
-conn_bts.exec(" SELECT * FROM ALL_TABLES")do |r|
+conn_bts.exec(" SELECT * FROM test")do |r|
   puts r.join(',')
 end
 
